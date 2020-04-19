57ab2694c12cc0804ed81ad5f0450312b9d335d8
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-19 23:18:09 +0530

    added daily wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,9 +2,14 @@
 # Employee Attendance
 echo "welcome to employye wage calculation"
 check=$(($RANDOM%2))
+wage_per_hr=20
+full_day_hr=8
+half_day_hr=4
 if [ $check -eq 1 ];
 then
         echo "employee is present"
+        echo "daily_wage=$(($wage_per_hr*$full_day_hr))"
 else
         echo "employee is absent"
+        echo "daily_wage=$(($wage_per_hr*0))"
 fi
