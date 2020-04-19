5896730708c2f3948e0679ab57f7de8d29c53166
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-20 00:06:25 +0530

    wage calculated till 100hrs

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,5 +1,5 @@
-#! /bin/bash -x
-#calculate wage for a month
+#! /bin/bash
+#Calculate Wages till a condition of total working hours or days is reached for a month
 echo "welcome to employye wage calculation"
 check=$(($RANDOM%2))
 wage_per_hr=20
@@ -8,25 +8,49 @@ half_day_hr=4
 total_work_day=20
 total_wage=0
 day_count=1
-while [ $day_count -le $total_work_day ]
+total_working_hrs=100
+work_hr=0
+while [ $day_count -le $total_work_day ] && [ $work_hr -lt $total_working_hrs ]
 do
         case $check in
                 1)
                 category=$(($RANDOM%2))
                 case $category in
                         1)
-                        echo "employee is present in day$day_count and is a full time_emp"
-                        daily_wage=$(($wage_per_hr*$full_day_hr))
-                        echo "daily_wage=$daily_ echo "
-                        total_wage=$(($total_wage+$daily_wage))
-                        day_count=$(($day_count+1))
+			if [ $work_hr -le 92 ];
+			then
+                        	echo "employee is present in day$day_count and is a full time_emp"
+                        	daily_wage=$(($wage_per_hr*$full_day_hr))
+                        	echo "daily_wage=$daily_ echo "
+                        	total_wage=$(($total_wage+$daily_wage))
+                        	day_count=$(($day_count+1))
+                        	work_hr=$(($work_hr+8))
+			else
+				echo "employee is present in day$day_count and is a full time_emp"
+                                daily_wage=$(($wage_per_hr*$((100-$work_hr))))
+                                echo "daily_wage=$daily_ echo "
+                                total_wage=$(($total_wage+$daily_wage))
+                                day_count=$(($day_count+1))
+                                work_hr=$(($work_hr+$((100-$work_hr))))
+			fi
                         ;;
                         *)
-                        echo "employee is present in day$day_count and is a part time_emp"
-                        daily_wage=$(($wage_per_hr*$half_day_hr))
-                        echo "daily_wage=$daily_wage"
-                        total_wage=$(($total_wage+$daily_wage))
-                        day_count=$(($day_count+1))
+			if [ $work_hr -le 96 ];
+			then
+                        	echo "employee is present in day$day_count and is a part time_emp"
+                        	daily_wage=$(($wage_per_hr*$half_day_hr))
+                        	echo "daily_wage=$daily_wage"
+                        	total_wage=$(($total_wage+$daily_wage))
+                        	day_count=$(($day_count+1))
+                        	work_hr=$(($work_hr+4))
+			else
+                                echo "employee is present in day$day_count and is a part time_emp"
+                                daily_wage=$(($wage_per_hr*$((100-$work_hr))))
+                                echo "daily_wage=$daily_wage"
+                                total_wage=$(($total_wage+$daily_wage))
+                                day_count=$(($day_count+1))
+                                work_hr=$(($work_hr+$((100-$work_hr))))
+			fi
                         ;;
                 esac
                 ;;
@@ -37,4 +61,5 @@ do
                 ;;
         esac
 done
-echo "monthly_wage=$total_wage"
+echo "total_wage=$total_wage"
+echo "total_work_hrs=$work_hr"
