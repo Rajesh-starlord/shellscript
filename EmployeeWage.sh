11e021491a80c2c3e3bdc65f338e821c4887e12d
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-19 23:29:07 +0530

    added part time to emp prog

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,15 +1,22 @@
 #! /bin/bash -x
-# Employee Attendance
+#add part time
 echo "welcome to employye wage calculation"
 check=$(($RANDOM%2))
 wage_per_hr=20
 full_day_hr=8
 half_day_hr=4
-if [ $check -eq 1 ];
+if [ $check -eq 1 ]
 then
-        echo "employee is present"
-        echo "daily_wage=$(($wage_per_hr*$full_day_hr))"
+        category=$(($RANDOM%2))
+        if [ $category -eq 1 ]
+        then
+                echo "employee is present and is a full time_emp"
+                echo "daily_wage=$(($wage_per_hr*$full_day_hr))"
+       	else
+                echo "employee is present and is a part time_emp"
+                echo "daily_wage=$(($wage_per_hr*$half_day_hr))"
+        fi
 else
-        echo "employee is absent"
+	echo "employee is absent"
         echo "daily_wage=$(($wage_per_hr*0))"
 fi
