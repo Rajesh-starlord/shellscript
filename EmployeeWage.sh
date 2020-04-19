3e7f238884cee1077f7b3eee8e2e81ac65a29281
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-19 23:34:51 +0530

    solved using switch case

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,22 +1,27 @@
 #! /bin/bash -x
-#add part time
+#solving Using Switch Case
 echo "welcome to employye wage calculation"
 check=$(($RANDOM%2))
 wage_per_hr=20
 full_day_hr=8
 half_day_hr=4
-if [ $check -eq 1 ]
-then
+case $check in
+        1)
         category=$(($RANDOM%2))
-        if [ $category -eq 1 ]
-        then
+        case $category in
+                1)
                 echo "employee is present and is a full time_emp"
                 echo "daily_wage=$(($wage_per_hr*$full_day_hr))"
-       	else
+                ;;
+                *)
                 echo "employee is present and is a part time_emp"
                 echo "daily_wage=$(($wage_per_hr*$half_day_hr))"
-        fi
-else
-	echo "employee is absent"
+                ;;
+        esac
+        ;;
+        *)
+        echo "employee is absent"
         echo "daily_wage=$(($wage_per_hr*0))"
-fi
+        ;;
+esac
+
