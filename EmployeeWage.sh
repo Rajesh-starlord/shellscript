06812e6b95d123f6745f2c699dc1b81a2c94fac2
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-19 23:09:54 +0530

    added use_case1

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,3 +1,10 @@
 #! /bin/bash -x
-#welcome message
+# Employee Attendance
 echo "welcome to employye wage calculation"
+check=$(($RANDOM%2))
+if [ $check -eq 1 ];
+then
+        echo "employee is present"
+else
+        echo "employee is absent"
+fi
